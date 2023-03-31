f76c4d73d2446cf449b394325820f1379b46bb1b
SaiVenkat12 <gsaiv1001@gmail.com>
2023-03-31 19:32:30 +0530

    [Sai Venkat]- Added-Employee Daily Wage Array in js

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,46 +1,45 @@
-const isPartTime=1;
-const isFullTime=0;
-const wagePerHr=20;
-const fullTimeHrs=8;
-const partTimeHrs=4;
-const empWorkingDays=20;
-const MaxHrs=100;
+const isPartTime = 1;
+const isFullTime = 0;
+const wagePerHr = 20;
+const fullTimeHrs = 8;
+const partTimeHrs = 4;
+const empWorkingDays = 20;
+const MaxHrs = 100;
 
-let empMonthlyWage=0;
-let empHrs=0;
-let totalDays=0;
-let totalEmpHrs=0;
-
-function MonthlyEmpWage()
-{
-    while(totalEmpHrs<MaxHrs && totalDays<empWorkingDays)
-    {
-        let check=Math.floor(Math.random()*10)%3;
+let empMonthlyWage = 0;
+let empHrs = 0;
+let totalDays = 0;
+let totalEmpHrs = 0;
+let empDailyWageArr = new Array();
+function MonthlyEmpWage() {
+    while (totalEmpHrs < MaxHrs && totalDays < empWorkingDays) {
+        let check = Math.floor(Math.random() * 10) % 3;
         totalDays++;
-        if(check==isPartTime)
-        {
-            console.log("Day -"+totalDays);
-            console.log("Employee -PartTime");
-            empHrs=partTimeHrs;
+        if (check == isPartTime) {
+            empHrs = partTimeHrs;
         }
-        else if(check==isFullTime)
-        {
-            console.log("Day -"+totalDays);
-            console.log("Employee -FullTime");
-            empHrs=fullTimeHrs;
+        else if (check == isFullTime) {
+            empHrs = fullTimeHrs;
         }
-        else
-        {
-            console.log("Day -"+totalDays);
-            console.log("Employee is Absent");
-            empHrs=0;
+        else {
+            empHrs = 0;
         }
-        totalEmpHrs+=empHrs;
-        let empWage=wagePerHr*empHrs;
-        console.log("Employee daily Wage = "+empWage);
-        empMonthlyWage +=empWage;
+        totalEmpHrs += empHrs;
+        let empWage = wagePerHr * empHrs;
+        empDailyWageArr.push(empWage);
+        //console.log("Employee daily Wage = "+empWage);
+        empMonthlyWage += empWage;
     }
-    console.log("Total hrs: "+totalEmpHrs+" Total Days: "+totalDays);
-    console.log("Monthly Wage = "+empMonthlyWage);
+    console.log("Total hrs: " + totalEmpHrs + " Total Days: " + totalDays);
+    console.log("Monthly Wage = " + empMonthlyWage);
+}
+function EmpWageArray() {
+    console.log("Daily Wage Array");
+    console.log(empDailyWageArr);
 }
 MonthlyEmpWage();
+EmpWageArray();
+
+
+
+
