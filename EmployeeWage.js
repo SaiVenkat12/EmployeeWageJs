9368920bbae5c2f7721f5cb5409f3cebbe920036
SaiVenkat12 <gsaiv1001@gmail.com>
2023-03-31 19:11:44 +0530

    [Sai Venkat]- Added-Employee Daily Wage in js

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,72 +3,23 @@ const isFullTime=0;
 const wagePerHr=20;
 const fullTimeHrs=8;
 const partTimeHrs=4;
-const empWorkingDays=20;
-const MaxHrs=100;
-
-let empMonthlyWage=0;
-let empHrs=0;
-let totalDays=0;
-let totalEmpHrs=0;
-let totalWage=0;
-let dayCount=0;
-let empDailyWageArr=new Array();
-let empDailyWageMap=new Map();
-
-function MonthlyEmpWage()
-{
-    while(totalEmpHrs<MaxHrs && totalDays<empWorkingDays)
+let check=Math.floor(Math.random()*10)%3;
+function EmpWage(){
+    let empHrs=0;
+    if(check==isPartTime)
     {
-        let check=Math.floor(Math.random()*10)%3;
-        totalDays++;
-        if(check==isPartTime)
-        {
-            empHrs=partTimeHrs;
-        }
-        else if(check==isFullTime)
-        {
-            empHrs=fullTimeHrs;
-        }
-        else
-        {
-            empHrs=0;
-        }
-        totalEmpHrs+=empHrs;
-        let empWage=wagePerHr*empHrs;
-
-        empDailyWageArr.push(empWage);
-        empDailyWageMap.set(totalDays,empWage);
-        //console.log("Employee daily Wage = "+empWage);
-        empMonthlyWage +=empWage;
+        console.log("Employee -PartTime");
+        empHrs=partTimeHrs;
     }
-    //console.log("Monthly Wage = "+empMonthlyWage);
-    //console.log(empDailyWageArr);
-    console.log(empDailyWageMap);
-    empDailyWageArr.forEach(DailyWageSum);
-    console.log(totalWage);
+    else if(check==isFullTime)
+    {
+        console.log("Employee -FullTime");
+        empHrs=fullTimeHrs;
+    }
+    else
+    console.log("Employee is Absent");
 
-    let empMapArr=empDailyWageArr.map(EmpMapWithWageAndDate);
-    //console.log(empMapArr);
-
-    let empFullWageArr=empMapArr.filter(EmpFullTimeWage);
-   //console.log(empFullWageArr);
-
-    //console.log("First time Full time Wage Eared");
-    //console.log(empMapArr.find(EmpFullTimeWage));
+    let empWage=wagePerHr*empHrs;
+    console.log("Employee daily Wage = "+empWage);
 }
-function DailyWageSum(wage){
-    totalWage+=wage;
-}
-function EmpMapWithWageAndDate(wage)
-{
-    dayCount++;
-    return dayCount+" - "+wage;
-}
-function EmpFullTimeWage(wage){
-    return wage.includes("160");
-}
-
-MonthlyEmpWage();
-
-
-
+EmpWage();
