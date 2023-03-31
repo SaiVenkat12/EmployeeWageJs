b66c5b3f7ea91d8cefb4a0ae01d20793bf93c2c7
SaiVenkat12 <gsaiv1001@gmail.com>
2023-03-31 19:15:25 +0530

    [Sai Venkat]- Added-Employee Monthly Wage in js

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,23 +3,37 @@ const isFullTime=0;
 const wagePerHr=20;
 const fullTimeHrs=8;
 const partTimeHrs=4;
-let check=Math.floor(Math.random()*10)%3;
+const empWorkingDays=20;
+let empMonthlyWage=0;
+let empHrs=0;
 function EmpWage(){
-    let empHrs=0;
-    if(check==isPartTime)
+    for(let i=1;i<=empWorkingDays;i++)
     {
-        console.log("Employee -PartTime");
-        empHrs=partTimeHrs;
-    }
-    else if(check==isFullTime)
-    {
-        console.log("Employee -FullTime");
-        empHrs=fullTimeHrs;
-    }
-    else
-    console.log("Employee is Absent");
+        let check=Math.floor(Math.random()*10)%3;
+        if(check==isPartTime)
+        {
+            console.log("Day -"+i);
+            console.log("Employee -PartTime");
+            empHrs=partTimeHrs;
+        }
+        else if(check==isFullTime)
+        {
+            console.log("Day -"+i);
+            console.log("Employee -FullTime");
+            empHrs=fullTimeHrs;
+        }
+        else
+        {
+            console.log("Day -"+i);
+            console.log("Employee is Absent");
+            empHrs=0;
+        }
 
-    let empWage=wagePerHr*empHrs;
-    console.log("Employee daily Wage = "+empWage);
+        let empWage=wagePerHr*empHrs;
+        console.log("Employee daily Wage = "+empWage);
+        empMonthlyWage +=empWage;
+    }
+    console.log("  Monthly Wage = "+empMonthlyWage);
+    
 }
 EmpWage();
